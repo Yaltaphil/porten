a92ca5c9738a99f8fea3d0bf77c8cc27accb3022
Yaltaphil <yaltaphil@yandex.ru>
2021-06-11 18:56:11 +0300

    fix: swiper, minor fixes

--- src/index.js
+++ src/index.js
@@ -3,29 +3,62 @@
 const burgermenu = document.querySelector(".burger-menu");
 const burger = document.querySelector(".second-line__burger");
 const closeburger = document.querySelector(".close-button");
+const body = document.body;
 
 burger.addEventListener("click", () => {
     burgermenu.classList.remove("hide");
     burgermenu.classList.add("show");
+    body.style.overflowY = "hidden";
 });
 
 closeburger.addEventListener("click", () => {
     burgermenu.classList.remove("show");
     burgermenu.classList.add("hide");
+    body.style.overflowY = "scroll";
 });
 
 const swiper1 = new Swiper(".mySwiper1", {
-    slidesPerView: 3,
     centeredSlides: true,
-    spaceBetween: 50,
     loop: true,
     autoplay: {
         delay: 3000,
     },
+
+    breakpoints: {
+        767: {
+            slidesPerView: 1,
+            allowTouchMove: true,
+        },
+        1108: {
+            allowTouchMove: true,
+            spaceBetween: 10,
+            slidesPerView: 3,
+        },
+        1499: {
+            allowTouchMove: true,
+            spaceBetween: 50,
+            slidesPerView: 3,
+        },
+    },
 });
 
 const swiper2 = new Swiper(".mySwiper2", {
-    slidesPerView: 4,
+    breakpoints: {
+        767: {
+            slidesPerView: 1,
+            allowTouchMove: true,
+        },
+        1108: {
+            allowTouchMove: true,
+            spaceBetween: 10,
+            slidesPerView: 3,
+        },
+        1499: {
+            allowTouchMove: true,
+            spaceBetween: 30,
+            slidesPerView: 3,
+        },
+    },
     centeredSlides: true,
     loop: true,
     autoplay: {
